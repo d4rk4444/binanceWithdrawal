aa5fb6cc192b28bcdd86c97629ff56d7f206cd4f
d4rk <ciml@protonmail.com>
2023-01-28 02:58:21 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { getCoinBalanceBinance, withdrawBinance } from 'tools-d4rk444/CEX.js';
-import { timeout, parseFile } from 'tools-d4rk444/other.js';
+import { timeout, parseFile, generateRandomAmount } from 'tools-d4rk444/other.js';
 import consoleStamp from 'console-stamp';
 import chalk from 'chalk';
 import * as dotenv from 'dotenv';
@@ -7,33 +7,24 @@ dotenv.config();
 
 consoleStamp(console, { format: ':date(HH:MM:ss)' });
 
-const generateRandomAmount = (min, max, num) => {
-    const amount = Number(Math.random() * (max - min) + min);
-    return Number(parseFloat(amount).toFixed(num));
-}
-
 (async() => {
     const wallets = parseFile('address.txt');
     const coin = process.env.COIN;
-    if (wallets.length == 0) {
-        console.log(chalk.red('Add address in file'));
-    } else {
-        await getCoinBalance(coin).then(async function(coinBalance) {
-            if (coinBalance == 0) {
-                console.log(chalk.red(`${coin} balance 0`));
-            } else {
-                for (let i = 0; i < wallets.length; i++) {
-                    if (wallets[i].length > 42) {
-                        console.log(chalk.red(`Incorrect address in ${i+1} line`))
-                    } else {
-                        const amount = generateRandomAmount(Number(process.env.AMOUNT_MIN), Number(process.env.AMOUNT_MAX), 5);
-                        await withdraw(coin, process.env.NETWORK, wallets[i], amount).then(function(id) {
-                            console.log(chalk.blue(`Send: ${amount}  toAddress: ${wallets[i]}  Id: ${id}`));
-                        });
-                        await timeout(5600);
-                    }
-                }
-            }
-        });
-    }
+    const pauseTime = generateRandomAmount(4500, 7800, 0);
+    const apiSecret = process.env.BINANCE_API_SECRET;
+    const apiKey = process.env.BINANCE_API_KEY;
+
+    if (wallets.length == 0) { throw new Error('No Address in file') };
+
+    await getCoinBalanceBinance(coin, apiSecret, apiKey).then(async(coinBalance) => {
+        if (coinBalance == 0) { throw new Error('Coin Balance = 0') }; 
+
+        for (let i = 0; i < wallets.length; i++) {
+            const amount = generateRandomAmount(Number(process.env.AMOUNT_MIN), Number(process.env.AMOUNT_MAX), 5);
+            await withdrawBinance(coin, process.env.NETWORK, wallets[i], amount, apiSecret, apiKey).then((id) => {
+                console.log(chalk.blue(`Send: ${amount}  toAddress: ${wallets[i]}  Id: ${id}`));
+            });
+            await timeout(pauseTime);
+        }
+    });
 })();
